d97e3f1490ca2ac94f2f4c6c365db9cceda4ea0d
Maximilian Du <32785271+MaxDu17@users.noreply.github.com>
2020-03-10 22:22:39 -0400

    worked on live action

---
+++ Live/LiveTest.py
@@ -0,0 +1,127 @@
+
+import serial
+import pickle
+
+import time
+from datacollector.ParserTool import Parser
+from serial.tools import list_ports
+from pipeline.Hyperparameters import Hyperparameters
+from pipeline.LiveTestParser import LiveParser
+import numpy as np
+from pipeline.MyCNNLibrary import * #this is my own "keras" extension onto tensorflow
+
+HYP = Hyperparameters()
+LiveHelp = LiveParser()
+
+
+version = "DownstairsCNN_" + HYP.MODE_OF_LEARNING
+
+weight_bias_list = list() #this is the weights and biases matrix
+
+base_directory = "../Graphs_and_Results/Vanilla" + "/" + version + "/"
+
+tool = Parser(False, "")
+
+
+try:
+    ser = serial.Serial(port='COM3', baudrate=115200)
+except:
+    print("sorry, this port is busy or not correct. double check programs!")
+    ports = list(list_ports.comports())
+    print("here are the available ports: " + str([k.device for k in ports]))
+    quit()
+
+
+def main():
+    holdinglist = list()
+    semantic = int(input("How many seconds do you want to wait for?"))
+    time.sleep(semantic)
+
+    idle_counter = 0
+
+    ser.reset_input_buffer()
+
+    buffer_size = LiveHelp.return_size_name(HYP.MODE_OF_LEARNING)
+
+
+    while True:
+        #print(count)
+        s = str(ser.readline())
+        frame = tool.extract_and_store(s)
+        idle_counter += 1
+
+        #print(s)
+        if(idle_counter > 8):
+            ser.close()
+            ser.open()
+            print("******************I just reset the serial connection*******************")
+
+
+        if(frame):
+            print("\t Frame Recorded")
+            holdinglist.append(frame.amplitude)
+
+            if len(holdinglist) == buffer_size:
+                feed_in = LiveHelp.process_from_raw(holdinglist, HYP.MODE_OF_LEARNING)
+                runNet(feed_in)
+
+
+
+class Model():
+    def __init__(self):
+        pool_size = (int(LiveHelp.return_size_name(HYP.MODE_OF_LEARNING) / 4) + 1) ** 2 * 8
+        self.cnn_1 = Convolve(weight_bias_list, [3, 3, 1, 4], "Layer_1_CNN")
+        self.cnn_2 = Convolve(weight_bias_list, [3, 3, 4, 4], "Layer_2_CNN")
+        self.pool_1 = Pool()
+
+        self.cnn_3 = Convolve(weight_bias_list, [3, 3, 4, 8], "Layer_2_CNN")
+        self.pool_2 = Pool()
+
+        self.flat = Flatten([-1, pool_size], "Fully_Connected")
+        self.fc_1 = FC(weight_bias_list, [pool_size, 5], "Layer_1_FC") #5 categories; change if necessary
+        self.softmax = Softmax()
+
+    def build_model_from_pickle(self, file_dir):
+        big_list = unpickle(file_dir)
+        # weights and biases are arranged alternating and in order of build
+        self.cnn_1.build(from_file=True, weights=big_list[0:2])
+        self.cnn_2.build(from_file=True, weights=big_list[2:4])
+        self.cnn_3.build(from_file=True, weights=big_list[4:6])
+        self.fc_1.build(from_file=True, weights=big_list[6:8])
+
+    def build_model(self):
+        self.cnn_1.build()
+        self.cnn_2.build()
+        self.cnn_3.build()
+        self.fc_1.build()
+
+    @tf.function
+    def call(self, input):
+        print("I am in calling {}".format(np.shape(input)))
+        x = self.cnn_1.call(input)
+        x = self.cnn_2.call(x)
+        x = self.pool_1.call(x)
+        x = self.cnn_3.call(x)
+        x = self.pool_2.call(x)
+
+        x = self.flat.call(x)
+        x = self.fc_1.call(x)
+        output = self.softmax.call(x)
+        return output
+
+def makeNet():
+    print("Making model")
+    model = Model()
+    model.build_model_from_pickle(base_directory + "SAVED_WEIGHTS.pkl")
+    return model
+
+def runNet(inVal, model):
+    prediction = model.call(inVal)
+    print("This is what the predicted value is: " + )
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ pipeline/LiveTestParser.py
@@ -0,0 +1,113 @@
+import numpy as np
+from os import listdir
+from pipeline.ProjectUtility import Utility
+
+class LiveParser(Utility):
+
+    def remove_gaps(self, data):
+        newData = list()
+        for frame in data:
+            carrier = frame[6:32]
+            carrier.extend(frame[33:59])
+            carrier = self.normalize_single_column(carrier)
+
+            carrier.extend(self.normalize_single_column(frame[66:123]))
+
+            carrier.extend(self.normalize_single_column(frame[134:191]))
+
+            newData.append(carrier)
+
+        return newData
+
+    def normalize_single_column(self, data):
+        min_ = min(data)
+        max_ = max(data)
+        for j in range(len(data)):
+            data[j] = (data[j] - min_) / (max_ - min_)
+
+        return data
+
+    def first_chunk(self, data):
+        newData = list()
+        for frame in data:
+            #carrier = frame[6:60] #30
+            carrier = frame[6:32]
+            carrier.extend(frame[33:59])
+            newData.append(carrier)
+
+        return newData
+
+    def second_chunk(self, data):
+        newData = list()
+        for frame in data:
+            carrier = frame[66:123]
+            newData.append(carrier)
+
+        return newData
+
+    def third_chunk(self, data):
+        newData = list()
+        for frame in data:
+            carrier = frame[134:191]
+            newData.append(carrier)
+
+        return newData
+
+    def normalize(self, data): #only run this after you have removed chunky bits
+        for i in range(len(data)):
+            min_ = min(data[i])
+            max_ = max(data[i])
+            for j in range(len(data[i])):
+                data[i][j] = (data[i][j]- min_)/(max_-min_)
+        return data
+
+
+    def process_from_raw(self, data, chunkIdentifier):
+        chunkNum = self.return_chunkIdent_name(chunkIdentifier)
+        assert len(data) == self.return_size_name(chunkIdentifier), "problem with array generation"
+        carrier = data
+        if chunkNum == 1:
+            return self.normalize(self.first_chunk(carrier))
+        elif chunkNum == 2:
+            return self.normalize(self.second_chunk(carrier))
+        elif chunkNum == 3:
+            return self.normalize(self.third_chunk(carrier))
+        elif chunkNum == 4:
+            return self.normalize(self.remove_gaps(carrier))
+        elif chunkNum == 0:
+            return self.normalize(carrier)
+        else:
+            raise Exception("Invalid chunkIdentifier")
+
+
+
+    def return_chunkIdent_name(self, chunkIdentifier):
+        if chunkIdentifier == "first":
+            return 1
+        elif chunkIdentifier == "second":
+            return 2
+        elif chunkIdentifier == "third":
+            return 3
+        elif chunkIdentifier == "all":
+            return 4
+        elif chunkIdentifier == "raw":
+            return 0
+
+    def return_size_name(self, chunkIdentifier):
+        size = 0
+        if chunkIdentifier == "first":
+            size = 52
+        elif chunkIdentifier == "second":
+            size = 57
+        elif chunkIdentifier == "third":
+            size = 57
+        elif chunkIdentifier == "all":
+            size = 166
+        elif chunkIdentifier == "raw":
+            size = 192
+
+        return size
+
+    def result_interpret(self, oneHot):
+        dictWhole = {}
+
